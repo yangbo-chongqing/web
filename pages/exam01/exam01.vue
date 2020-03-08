c674640b9d6088ecb48468e44f799ba04adf5ede
yangbo <812507557@qq.com>
2020-03-08 10:56:38 +0800

    测试

---
+++ pages/exam01/exam01.vue
@@ -0,0 +1,70 @@
+<template>
+	<view>
+		
+		<!-- 列表样式 -->
+		<view class="p-2">
+			<!-- 头像昵称 | 关注按钮 -->
+			<view class="flex align-center justify-between">
+				<view class="flex align-center">
+					<!-- 头像 -->
+					<image class="rounded-circle" src="/static/default.jpg" style="width: 65rpx;height: 65rpx;margin-right: 20rpx;" lazy-load></image>
+					<!-- 昵称发布时间 -->
+					<view>
+						<view style="font-size: 30rpx;line-height: 1.5;">昵称</view>
+						<text style="color: #9D9589;font-size: 25rpx;line-height: 1.5;">2019-10-20 下午04:30</text>
+					</view>
+				</view>
+				<!-- 按钮 -->
+				<view class="flex align-center justify-center" style="width: 90rpx;height: 50rpx;border-radius: 5rpx;background-color: #FF4A6A;color: #FFFFFF;">
+					关注
+				</view>
+			</view>
+			<!-- 标题 -->
+			<view style="font-size: 30rpx; margin: 10rpx 0;">
+				我是标题
+			</view>
+			<!-- 图片 -->
+			<image src="/static/demo/datapic/11.jpg" style="height: 350rpx;width: 100%;border-radius: 5rpx;"></image>
+			<!-- 图标按钮 -->
+			<view class="flex align-center">
+				<view class="flex align-center justify-center flex-1">
+					<text class="iconfont icon-dianzan2" style="margin-right: 20rpx;"></text>
+					<text>1</text>
+				</view>
+				<view class="flex align-center justify-center flex-1">
+					<text class="iconfont icon-cai" style="margin-right: 20rpx;"></text>
+					<text>1</text>
+				</view>
+				<view class="flex align-center justify-center flex-1">
+					<text class="iconfont icon-pinglun2" style="margin-right: 20rpx;"></text>
+					<text>1</text>
+				</view>
+				<view class="flex align-center justify-center flex-1">
+					<text class="iconfont icon-fenxiang" style="margin-right: 20rpx;"></text>
+					<text>1</text>
+				</view>
+			</view>
+		</view>
+		
+		
+	</view>	
+	
+	<!--view>
+		<view style="display: flex;justify-content: center;padding:50rpx;">
+	<view  class="animated" hover-class="pulse" 
+	style="border:1px solid #333333;padding:20rpx;">
+		点击效果
+	</view>
+	</view>
+	<text class="iconfont icon-smile"></text>
+	<text class="iconfont icon-kulian"></text>
+	<text class="iconfont icon-xinlangweibo"></text>
+	
+	</view-->
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+	<div>
+		<span>混编入门示例{{msg}}</span>
+	</div>
+	<a href="http://www.baidu.com">去百度</a>
+	<image src="../../static/logo.png" style="width:100rpx;height:200rpx;"></image>
+	<view :class="{ active: isActive }">111</view>
+	<view class="static" v-bind:class="{ active: isActive, 'text-danger': hasError }">222</view>
+	
+	 <view v-for="(item,index) in rows" :key="item.id">
+	            {{index +':'+ item.name}}
+	 </view>
+	<div id="example">
+	  <p>原数据: "{{ message }}"</p>
+	  <p>返转后数据: "{{ reversedMessage }}"</p>
+	</div>
+	<h1 v-if="awesome">Vue is awesome!</h1>
+	<h1 v-else>Oh no 😢</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				msg:"我的心",
+				isActive:true,
+				hasError:true,
+				rows:[{name:"东哥"},{name:"亮亮"}],
+				message:"东哥",
+				awesome:true
+			}
+		},
+		computed: {
+		    // 计算属性的 getter
+		    reversedMessage: function () {
+		      // `this` 指向 vm 实例
+		      return this.message.split('').reverse().join('')
+		    }
+		  }
+	}
+</script>
+
+<style>
+	.active{
+		color:red;
+		
+	}
+	.text-danger{
+		background-color: #0066CC;
+	}
+</style>
